bce5201e490089141c9e04341cf97c9d3c73ab8e
netaru3 <netaru3@gmail.com>
2025-07-25 22:20:31 -0300

    Add files via upload

---
+++ alarma.js
@@ -0,0 +1,8 @@
+function alarma() {const id=setInterval(function set(){let fecha= new Date().toLocaleTimeString([], {
+  hour: '2-digit',
+  minute: '2-digit'});
+let fecha2= document.getElementById("timer").value;
+if(fecha===fecha2){document.getElementById("alarmasonido").play();alert("Sonó su alarma");
+    clearInterval(id);document.getElementById("alarmasonido").pause()
+}},1000)}
+//debo crear un setInterval()
