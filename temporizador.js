6455152a9b29cb057ed86659721ebcb86f328478
netaru3 <netaru3@gmail.com>
2025-07-26 16:53:09 -0300

    Update temporizador.js

--- temporizador.js
+++ temporizador.js
@@ -1,8 +1,20 @@
 let remoboton= document.getElementById("boton")
+
+//function temporizadorUwU(){let temp=Number(document.getElementById("temporizador").value);
+  //  let minutos= Number(document.getElementById("minutos").value);
+   //let id= setInterval(function(){if((temp+ minutos*60)>=0){--temp;
+     //   document.getElementById("tiem").textContent=temp+ minutos*60};
+    //if((temp+ minutos*60)==0){document.getElementById("alarmasonido").play();
+//clearInterval(id);alert("El tiempo ha terminado"); document.getElementById("alarmasonido").pause()
+  //  }},1000); remoboton.remove()
+//}
 function temporizadorUwU(){let temp=Number(document.getElementById("temporizador").value);
+    let minutos= Number(document.getElementById("minutos").value);
    let id= setInterval(function(){if(temp>=0){--temp;
-        document.getElementById("tiem").textContent=temp};
-    if(temp==0){document.getElementById("alarmasonido").play();
+        document.getElementById("seconds").textContent=temp;
+    document.getElementById("minuts").textContent=minutos;
+if(temp==0 && minutos!=0){--minutos; temp=temp+60}};
+    if((temp+ minutos*60)==0){document.getElementById("alarmasonido").play();
 clearInterval(id);alert("El tiempo ha terminado"); document.getElementById("alarmasonido").pause()
     }},1000); remoboton.remove()
 }
