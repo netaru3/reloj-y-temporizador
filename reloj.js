183b2149ce09e5bb4d0dcefe01044828b594247a
Aarón <netaru3@gmail.com>
2025-07-26 17:39:12 -0300

    Update reloj.js

--- reloj.js
+++ reloj.js
@@ -1,5 +1,9 @@
  tiempo= new Date()
 let i=0
-setInterval(function() {let tiempo=new Date();let ahora=tiempo.toLocaleTimeString();
+setInterval(function() {let tiempo=new Date();let ahora=tiempo.toLocaleTimeString([], {
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+hour12: false});
     document.getElementById("tiempo").textContent=ahora
 },1000)
