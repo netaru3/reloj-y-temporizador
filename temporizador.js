3a39d259b74bb739064e3263bf9fbc074e634f10
netaru3 <netaru3@gmail.com>
2025-07-25 22:21:31 -0300

    Update temporizador.js

--- temporizador.js
+++ temporizador.js
@@ -2,5 +2,7 @@ let remoboton= document.getElementById("boton")
 function temporizadorUwU(){let temp=Number(document.getElementById("temporizador").value);
    let id= setInterval(function(){if(temp>=0){--temp;
         document.getElementById("tiem").textContent=temp};
-    if(temp==0){clearInterval(id);alert("El tiempo ha terminado")}},1000); remoboton.remove()
+    if(temp==0){document.getElementById("alarmasonido").play();
+clearInterval(id);alert("El tiempo ha terminado"); document.getElementById("alarmasonido").pause()
+    }},1000); remoboton.remove()
 }
