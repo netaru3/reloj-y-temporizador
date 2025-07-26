aeae12b2c622d731b26eb760fa3ff5635e102c21
Aarón <netaru3@gmail.com>
2025-07-26 19:02:14 -0300

    Update alarma.js

--- alarma.js
+++ alarma.js
@@ -1,11 +1,12 @@
-function alarma() {const id=setInterval(function set(){let fecha= new Date().toLocaleTimeString([], {
+function alarma() {let fecha2= document.getElementById("timer").value;
+    if(fecha2==""){alert("ingrese un tiempo en la alarma"); return}
+;const id=setInterval(function set(){let fecha= new Date().toLocaleTimeString([], {
   hour: '2-digit',
   minute: '2-digit',
 hour12: false});
-let fecha2= document.getElementById("timer").value;
 if(fecha===fecha2){document.getElementById("alarmasonido").play();alert("Sonó su alarma");
-    clearInterval(id);document.getElementById("alarmasonido").pause();
-    
+    clearInterval(id);document.getElementById("alarmasonido").pause(); 
 }
-document.getElementById("boton2").remove()},1000)}
+document.getElementById("boton2").remove();
+},1000)}
 //debo crear un setInterval()
