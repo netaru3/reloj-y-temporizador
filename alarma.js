d0fb2bd22bd5888402cb09fe4f36c1c0733491ee
netaru3 <netaru3@gmail.com>
2025-07-26 01:27:34 -0300

    Update alarma.js

--- alarma.js
+++ alarma.js
@@ -1,6 +1,7 @@
 function alarma() {const id=setInterval(function set(){let fecha= new Date().toLocaleTimeString([], {
   hour: '2-digit',
-  minute: '2-digit'});
+  minute: '2-digit',
+hour12: false});
 let fecha2= document.getElementById("timer").value;
 if(fecha===fecha2){document.getElementById("alarmasonido").play();alert("Sonó su alarma");
     clearInterval(id);document.getElementById("alarmasonido").pause()
